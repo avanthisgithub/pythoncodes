7ef8b736298d6a2e7427b9982d9800e3ba5a2ace
avanthisurendra <avanthi.surendra14@gmail.com>
2024-06-25 16:58:13 +0530

    Create README.md

--- app.py
+++ app.py
@@ -1,2 +1,7 @@
-x = 2.5
-print(round(x))
+name = input("Enter you name: ")
+if len(name) < 3:
+    print("Name must be at least 3 characters")
+elif len(name) > 50:
+    print("Name can be maximum of 50 characters")
+else:
+    print("Name looks good!")
