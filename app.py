4d00ee1ef1bd032a343c5f3ab4d6d293ddc4a73a
avanthisurendra <avanthi.surendra14@gmail.com>
2024-07-12 08:56:45 +0530

    Create README.md

--- app.py
+++ app.py
@@ -1,27 +1,16 @@
-#car game
-i = 1
-while i <= i:
-    i = i + 1
-    command = str(input(">"))
-    if command.lower() == 'help':
-        print("start - to start the car")
-        print("stop - to stop thw car")
-        print("quit - to exit")
-    elif command.lower() == 'quit':
-        break
-    elif command.lower() == 'start':
-        print("Car started... Ready to go!")
-    elif command.lower() == 'stop':
-        print("Car stopped.")
-    elif command != 'quit' or 'start' or 'stop' or 'help':
-        print("I don't understand that...")
-
-
-
-
-
-
-
+temperature = input("Enter the temperature: ")
+if int(temperature) >= 30:
+    print("It id a hot day!"
+          "Stay hydrated")
+elif 10 < int(temperature) <= 20:
+    print("It is a cold day!"
+          "Wear more warm clothes")
+elif int(temperature) <= 10:
+    print("It is a very cold day!"
+          "Wear woolen clothes")
+elif 20 < int(temperature) < 30:
+    print("It is a wonderful day!"
+          "Enjoy your day!")
 
 
 
