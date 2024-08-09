44b92591924d4d9b45a110596e40d3e21804587b
avanthisurendra <avanthi.surendra14@gmail.com>
2024-08-09 10:50:06 +0530

    Create README.md

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
-temperature = input("Enter the temperature: ")
-if int(temperature) >= 30:
-    print("It id a hot day!"
-          "Stay hydrated")
-elif 10 < int(temperature) <= 20:
-    print("It is a cold day!"
-          "Wear more warm clothes")
-elif int(temperature) <= 10:
-    print("It is a very cold day!"
-          "Wear woolen clothes")
-elif 20 < int(temperature) < 30:
-    print("It is a wonderful day!"
-          "Enjoy your day!")
+prices = [45, 38, 84]
+total = 0
+for price in prices:
+    total = total + price
+print(f"the total is {total}")
+
+
+
+
+
+
+
+
 
 
 
